27d15ea97b52896cff72f549f44e427eaab717ca
Tigre-Bleu <devel@tigre-bleu.net>
2024-04-23 16:22:29 +0200

    Fix variable name

--- templates/gitlab.rb
+++ templates/gitlab.rb
@@ -1,10 +1,10 @@
-external_url '{{ gitlab_url }}'
+external_url '{{ gitlab.url }}'
 gitlab_rails['smtp_enable'] = true
-gitlab_rails['smtp_address'] = "{{ gitlab_smtp }}"
+gitlab_rails['smtp_address'] = "{{ gitlab.smtp }}"
 gitlab_rails['smtp_port'] = 25
 gitlab_rails['smtp_domain'] = "localhost"
 gitlab_rails['smtp_enable_starttls_auto'] = false
 gitlab_rails['smtp_tls'] = false
-gitlab_rails['gitlab_email_from'] = '{{ gitlab_email_from }}'
-gitlab_rails['gitlab_email_display_name'] = '{{ gitlab_displayname }}'
-gitlab_rails['gitlab_email_reply_to'] = '{{ gitlab_replyto }}'
+gitlab_rails['gitlab_email_from'] = '{{ gitlab.email_from }}'
+gitlab_rails['gitlab_email_display_name'] = '{{ gitlab.displayname }}'
+gitlab_rails['gitlab_email_reply_to'] = '{{ gitlab.replyto }}'
